c133bb5d89aa9f213bb9f3a624fcb5aee0992d03
marcosFerreiraMoura <marcosf.sousa100@gmail.com>
2025-08-18 22:14:09 -0300

    "atualizaçoes"

--- analiseSentimentos.py
+++ analiseSentimentos.py
@@ -11,21 +11,15 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.metrics import accuracy_score
 import pickle
 
-
-data = pd.read_csv(r'data\imdb-reviews-pt-br.csv')
+def carregar_dados():
+    data = pd.read_csv(r'data\imdb-reviews-pt-br.csv')
+    return data
 #data = data[['review']]
-
-print(data.shape)
-data.head()
-
-data.info()
-
-#print(data.text_en[0])
 
 def clean(text):
     cleaned = re.compile(r'<.*?>')
     return re.sub(cleaned,'',text)
-data.text_en = data.text_en.apply(clean)
+#carregar_dados().text_en = carregar_dados().text_en.apply(clean)
 
 #print('\n\n Remove tags HTMLs\n' + reviewCleaned)
 
@@ -38,13 +32,13 @@ def is_special(text):
         else:
             rem = rem + ' '
     return rem
-data.text_en = data.text_en.apply(is_special)
+#carregar_dados().text_en = carregar_dados().text_en.apply(is_special)
 #print('\n\n Remove caracteres especiais\n' + semCharEspeciais )
 
 #convertendo para lowCase
 def to_lower(text):
     return text.lower()
-data.text_en = data.text_en.apply(to_lower)
+#carregar_dados().text_en = carregar_dados().text_en.apply(to_lower)
 #print('\n\n coloca texto em minusculo\n'+ lowCase)
 
 
@@ -57,7 +51,7 @@ def rem_stopwords(text):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text)
     return[w for w in words if w not in stop_words]
-data.text_en = data.text_en.apply(rem_stopwords)
+#carregar_dados().text_en = carregar_dados().text_en.apply(rem_stopwords)
 #print('\n\n Remover Artigos\n'+' '.join(limpaArtTxt))
 
 #
@@ -65,48 +59,67 @@ def stem_txt(text):
     ss = SnowballStemmer('english')
     return " " .join([ss.stem(w) for w in text])
 
-data.text_en = data.text_en.apply(stem_txt)
+#carregar_dados().text_en = carregar_dados().text_en.apply(stem_txt)
 
 #print('\n\n tratar texto, reduzir palavras\n')
 
-#Maquina preditiva
-#criando uma bag of words
+# ====== Treinamento (só roda se o arquivo for executado diretamente) ======
+if __name__ == "__main__":
+    data = carregar_dados()
+
+    # Pré-processamento
+    data["text_en"] = data["text_en"].apply(clean)
+    data["text_en"] = data["text_en"].apply(is_special)
+    data["text_en"] = data["text_en"].apply(to_lower)
+    data["text_en"] = data["text_en"].apply(rem_stopwords)
+    data["text_en"] = data["text_en"].apply(stem_txt)
+    print(data.shape)
+    data.head()
+    data.info()
 
 
-#x = np.array(data.iloc[:,0].values)
-y = np.array(data.sentiment.values)
-cv = CountVectorizer(max_features= 1000)
-x = cv.fit_transform(data.text_en).toarray()
+    #Maquina preditiva
+    #criando uma bag of words
 
 
-print("X shape = ", x.shape)
-print("Y shape = ", y.shape)
-#print(y)
-trainx, testx, trainy, testy = train_test_split(x, y, test_size= 0.2,random_state= 9 )
-print("Train shapes: X = {}, y ={} ".format(trainx.shape, trainy.shape))
-print("Test shapes: X = {}, y ={} ".format(testx.shape, testy.shape))
-
-#Definindo modelos e treinando eles
-
-gnb, mnb, bnb = GaussianNB(), MultinomialNB(alpha=1.0, fit_prior=True), BernoulliNB(alpha=1.0, fit_prior=True)
-
-gnb.fit(trainx, trainy)
-mnb.fit(trainx, trainy)
-bnb.fit(trainx, trainy)
-
-#predicao e acuracia para escolher o melhor modelo
-
-ypg = gnb.predict(testx)
-ypm = mnb.predict(testx)
-ypb = bnb.predict(testx)
-
-print("Gaussian = ",accuracy_score(testy, ypg))
-print("Multinominal= ",accuracy_score(testy, ypm))
-print("Bernoulli = ",accuracy_score(testy, ypb))
-
-#salva o modelo treinado
-pickle.dump(bnb, open('model0.pk1','wb'))
-# Salvar o CountVectorizer
-pickle.dump(cv, open('cv.pk1', 'wb'))
+    # bag of words
+    #x = np.array(data.iloc[:,0].values)
+    y = np.array(data.sentiment.values)
+    cv = CountVectorizer(max_features= 1000)
+    x = cv.fit_transform(data.text_en).toarray()
 
 
+    print("X shape = ", x.shape)
+    print("Y shape = ", y.shape)
+    #print(y)
+    #divisao de treinos, 20% pra teste e o restante pra treino
+    trainx, testx, trainy, testy = train_test_split(x, y, test_size= 0.2,random_state= 9 )
+    print("Train shapes: X = {}, y ={} ".format(trainx.shape, trainy.shape))
+    print("Test shapes: X = {}, y ={} ".format(testx.shape, testy.shape))
+
+    #Definindo modelos 
+
+    gnb, mnb, bnb = GaussianNB(), MultinomialNB(alpha=1.0, fit_prior=True), BernoulliNB(alpha=1.0, fit_prior=True)
+
+    gnb.fit(trainx, trainy)
+    mnb.fit(trainx, trainy)
+    bnb.fit(trainx, trainy)
+
+    #predicao e acuracia para escolher o melhor modelo
+
+    ypg = gnb.predict(testx)
+    ypm = mnb.predict(testx)
+    ypb = bnb.predict(testx)
+
+    #escolher o que mais alto score tiver
+    print("Gaussian = ",accuracy_score(testy, ypg))
+    print("Multinominal= ",accuracy_score(testy, ypm))
+    print("Bernoulli = ",accuracy_score(testy, ypb))
+
+    #salva o modelo treinado
+    pickle.dump(bnb, open('model0.pk1','wb'))
+    # Salvar o CountVectorizer
+    pickle.dump(cv, open('cv.pk1', 'wb'))
+
+
+ 
--- teste.py
+++ teste.py
@@ -4,54 +4,28 @@ from analiseSentimentos import clean, is_special, to_lower, rem_stopwords,stem_t
 cv = pickle.load(open('cv.pk1', 'rb'))
 bnb = pickle.load(open('model0.pk1', 'rb'))
 
+
+def prever_sentimento(texto, cv, modelo):
+    #pre- processamento
+    f1 = clean(texto)
+    f2 = is_special(f1)
+    f3 = to_lower(f2)
+    print(f3)
+    f4 = rem_stopwords(f3)
+    f5 = stem_txt(f4)
+
+    vetor =  cv.transform([f5]).toarray()
+    return modelo.predict(vetor)
+
+#fazendo o teste
+
 rev = """
-More so than with any other film I've watched in recent memory, I rather don't entirely know what to say after watching this. There's a stark, 
-jolting precision and brutality to much of the movie, even well preceding the abject violence, yet also a weirdly poetic beauty at many points. 
-This is as true for Lindsay Anderson's direction as it is for David Sherwin's screenplay, in all ways, but also for the acting. Performances are 
-exact and practiced, but also fluid and natural. Whether presented in pure black and white, or in color under drab skies or sunny blue, the very 
-image before us and cinematography is rich and lush. For viewers such as myself whose perspective on schooling in the United Kingdom is informed 
-wholly by cinematic exhibition and not personal experience, the strict regimentation and forced social arrangements are both fascinating and 
-uninviting - to say nothing of what embellishments the movie makes in imparting its tale. When all is said and done, the result is that for any 
-similarities one could find to this, that, or the other thing in more than 50 years since, 'If....' still feels quite unlike anything else.
+The show is set in a future where giant spaceships are manufactured and sent out beyond the Earth into deep space and there is no mechanism to detect when one of those ships is hurtling back towards Earth? No defence mechanism? At least in the Expanse the rock hurtling people had to figure out a way to disable the defences. Lazy and contrived.
 
-For all the pomp and circumstance and plays for power and social position, and the inherent fictional nature of the feature, there's an earnestness 
-to every aspect - characters, dialogue, scene writing, narrative, direction, performances - that comes off as very real, organic and relatable. It's 
-an enticing balance maintained at all times between various moods and tones, with the interactions between characters taking foremost precedence as 
-a focal point and anchor, whether trending toward antagonism or camaraderie. And with that said, not to belabor the point, but the contributions of 
-the actors seems particularly essential in 'If....' to cementing the picture. As I've suggested, I think everyone on hand does a fine job of helping 
-to bring the story to life with portrayals of nuance, poise, and personality, yet this goes above all for those whose characters are ultimately dubbed 
-the "crusaders." While credited alongside those more prominent, Rupert Webster and Christine Noonan have little more than bit parts as Philips and 
-"the girl"; we know so little about their characters, and one wishes they could have been fleshed out more at least to solidify motivations. Still, 
-Webster and Noonan make strong impressions despite their limited time on screen. David Wood and Richard Warwick are decidedly more visible as Knightly 
-and Wallace, and both actors do well in embodying the sneering disregard of the boys. But of course it's unmistakable Malcolm McDowell, starring as 
-protagonist Mick Travis, who stands out most of all. There are subtleties in McDowell's distinct vocal timbre, and in his expressions and body language, 
-that communicate definite confidence, defiance, and attitude, and just as it's hard to imagine anyone else as Alex DeLarge in 'A Clockwork Orange,' he 
-is a perfect fit to depict the boiling malcontent of young Travis.
+And not sure if that's worse or better than the ship crash lands on Earth and they send children in synthetic bodies with no hazmat team? No attempt to contain or quarantine before. Just go right in there and start exploring guys no need to worry about what might be in there!!
 
-I don't feel that it's perfect. As well made as it is, and as enjoyable as the viewing experience is, there's a part of me that think maybe my perception 
-of shortcomings is actually just an inability to glean the artistic choice behind certain inclusions. Again speaking to the characters of Philips and the 
-girl - we're given minimal information of them generally, and little or nothing that would meaningfully serve to explain their participation in the finale. 
-Jute is given a fair amount of screen time early on, then wordlessly fades from the narrative. One could infer to a reasonable certainty the significance 
-of a specific scene featuring Mrs. Kemp, but in the end it just seems superfluous to the whole. Broadly speaking, it just seems like the writing could have 
-stood to be a little tighter and more concrete; by no means does this completely dampen the value, but it's a notable aspect of the production.
+Pahleez.
 
-Subjective faults notwithstanding, however - by and large, 'If....' is pretty fantastic. I'm not sure that it totally met my expectations based on what 
-little I had read of it, but for the most part, I'm glad to have been surprised. It's a wonderfully subversive story of individuality and discontent set 
-against the rigidity and corruption of the establishment, and it's presented with a refined touch behind almost every element. Even if something about the 
-feature feels a little off, and not fully copacetic, that sense is minor in comparison to the engrossing drama to play out. Minding content warnings for 
-violence and nudity, this isn't going to be for everyone, but I think it's solid enough that I'd have no qualms about recommending it to just about anyone. 
-Though perhaps not altogether essential, 'If....' is an excellent, satisfying picture that's worth checking out if one has the opportunity.
 """
-f1 = clean(rev)
-
-f2 = is_special(f1)
-f3 = to_lower(f2)
-print('F1: ', f3)
-f4 = rem_stopwords(f3)
-f5 = stem_txt(f4)
-
-
-inp =  cv.transform([f5]).toarray()
-y_pred = bnb.predict(inp)
-
+y_pred = prever_sentimento(rev, cv, bnb )
 print("predict teste: ", y_pred)
